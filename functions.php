64d9ac1cf8d6b3168a388021d45624942a222605
shunya <shunyagoto192@gmail.com>
2022-09-12 19:18:56 +0900

    WP

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
 
-function add_files()
-{
-  // main.js
-  wp_enqueue_script('jq', get_theme_file_uri('/js/jquery-3.6.1.min.js'), array('jquery'));
-  wp_enqueue_script('main-js', get_theme_file_uri('/js/main.js'), 'jquery', '1.0.0', true);
-  //リセットcss
-  wp_enqueue_style('reset-css', get_theme_file_uri('/css/modern-css-reset-master/src/reset.css'), array(), '1.4.0');
-  //メインのcssファイル
-  wp_enqueue_style('style-css', get_stylesheet_uri(), array(), '1.0.0');
-}
-add_action('wp_enqueue_scripts', 'add_files', 'readScript');
+// function add_files()
+// {
+//   // main.js
+//   wp_enqueue_script('jq', get_theme_file_uri('/js/jquery-3.6.1.min.js'), array('jquery'));
+//   wp_enqueue_script('main-js', get_theme_file_uri('/js/main.js'), 'jquery', '1.0.0', true);
+//   //リセットcss
+//   wp_enqueue_style('reset-css', get_theme_file_uri('/css/modern-css-reset-master/src/reset.css'), array(), '1.4.0');
+//   //メインのcssファイル
+//   wp_enqueue_style('style-css', get_stylesheet_uri(), array(), '1.0.0');
+// }
+// add_action('wp_enqueue_scripts', 'add_files', 'readScript');
 
 ?>
