4c62757c445c53b15f05372d8bbea1804af26a96
shunya <shunyagoto192@gmail.com>
2022-09-18 11:22:47 +0900

    submit2

--- 404.php
+++ 404.php
@@ -1,1 +1,13 @@
-<p>aaa<p>
+<?php get_header(); ?>
+
+<h2 class="c-txt-headline__error">404 NOT FOUND<span>ERROR</span></h2>
+<div class="main">
+  <div class="l-container__error">
+    <p>お探しのページが見つかりませんでした。</p>
+    <p>申し訳ございませんが、<a href="<?php echo esc_url(home_url('/')); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
+  </div>
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
