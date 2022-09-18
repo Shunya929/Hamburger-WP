42ac54ca0d0f29e9a0714a25120f1859426da88d
shunya <shunyagoto192@gmail.com>
2022-09-18 11:10:33 +0900

    submit2

--- front-page.php
+++ front-page.php
@@ -7,32 +7,32 @@
       <ul class="p-menu-nav">
         <li class="l-container--first">
           <div class="c-text__main">
-            <h2 class="c-txt-headeline__menunav">Take Out</h2><br>
+            <a href="<?php echo esc_url(home_url('/category/takeout/')); ?>" class="c-txt-headeline__menunav">Take Out</a><br>
             <div class="c-underline--first"></div>
           </div>
           <ul class="c-textbox">
             <li class="c-textbox--main">
-              <h3 class="c-txt-headline__textbox">Take Out</h3>
+              <a href="<?php echo esc_url(home_url('/category/takeout/')); ?>" class="c-txt-headline__textbox">Take Out</a>
               <p class="c-txt-description__textbox">当店のテイクアウトで利用できる商品を掲載しています 当店のテイクアウトで利用できる商品を掲載しています 当店のテイクアウトで利用できる商品を掲載しています</p>
             </li>
             <li class="c-textbox--main">
-              <h3 class="c-txt-headline__textbox">Take Out</h3>
+              <a href="<?php echo esc_url(home_url('/category/takeout/')); ?>" class="c-txt-headline__textbox">Take Out</a>
               <p class="c-txt-description__textbox">当店のテイクアウトで利用できる商品を掲載しています 当店のテイクアウトで利用できる商品を掲載しています 当店のテイクアウトで利用できる商品を掲載しています</p>
             </li>
           </ul>
         </li>
         <li class="l-container--second">
           <div class="c-text__main">
-            <h2 class="c-txt-headeline__menunav">Eat In</h2><br>
+            <a href="<?php echo esc_url(home_url('/category/eatin/')); ?>" class="c-txt-headeline__menunav">Eat In</a><br>
             <div class="c-underline--first"></div>
           </div>
           <ul class="c-textbox">
             <li class="c-textbox--main">
-              <h3 class="c-txt-headline__textbox">Eat In</h3>
+              <a href="<?php echo esc_url(home_url('/category/eatin/')); ?>" class="c-txt-headline__textbox">Eat In</a>
               <p class="c-txt-description__textbox">店内でお食事いただけるメニューです 店内でお食事いただけるメニューです 店内でお食事いただけるメニューです</p>
             </li>
             <li class="c-textbox--main">
-              <h3 class="c-txt-headline__textbox">Eat In</h3>
+              <a href="<?php echo esc_url(home_url('/category/eatin/')); ?>" class="c-txt-headline__textbox">Eat In</a>
               <p class="c-txt-description__textbox">店内でお食事いただけるメニューです 店内でお食事いただけるメニューです 店内でお食事いただけるメニューです</p>
             </li>
           </ul>
